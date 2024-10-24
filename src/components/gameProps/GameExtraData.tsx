296223ddc04e946537f3413868a1096913c91f46
bojanpopov <bojanpopov94@gmail.com>
2024-10-24 14:53:12 +0200

    removed unnecasary console.log

--- src/components/gameProps/GameExtraData.tsx
+++ src/components/gameProps/GameExtraData.tsx
@@ -14,7 +14,6 @@ const GameExtraData = ({ ticker }: Props) => {
   const [selectedStat, setSelectedStat] = useState<string>('spread');
   const [chartDataHome, setChartDataHome] = useState<any>({});
   const [chartDataAway, setChartDataAway] = useState<any>({});
-  console.log(ticker, 'see this');
   const handleSelectStats = (id: string) => {
     return setSelectedStat(id);
   };
